cb6e08ff4674a054678c793d8157bb1547e262a8
FreekManders <freek.manders@gmail.com>
2020-04-09 14:54:44 +0200

    Added checks for presence of indels.

--- R/check_remove_muttypes.R
+++ R/check_remove_muttypes.R
@@ -8,7 +8,6 @@
 #' @param grl GRanges/GRangesList object
 #' 
 #' @return Invisibly returns its input when it succeeds.
-#' @examples
 #' 
 #' @importFrom magrittr %>%
 
@@ -34,7 +33,6 @@ check_no_multi_alts = function(grl){
 #' @param gr GRanges object
 #' 
 #' @return Invisibly returns its input when it succeeds.
-#' @examples
 #' 
 #' @importFrom magrittr %>%
 
@@ -44,7 +42,7 @@ check_no_multi_alts_gr = function(gr){
         unlist() %>% 
         length()
     if (length(gr) != nr_alts){
-        stop("There should not be any variants with multiple alternative alleles. Please remove these variants with remove_multi_alts_variants.", call. = F)
+        stop("There should not be any variants with multiple alternative alleles. You can remove these by using the `get_mut_type` function.", call. = F)
     }
     invisible(gr)
 }
@@ -58,7 +56,6 @@ check_no_multi_alts_gr = function(gr){
 #' @param grl GRanges/GrangesList object
 #' 
 #' @return Invisibly returns its input when it succeeds.
-#' @examples
 #' 
 
 check_no_snvs = function(grl){
@@ -82,19 +79,61 @@ check_no_snvs = function(grl){
 #' @param gr GRanges object
 #' 
 #' @return Invisibly returns its input when it succeeds.
-#' @examples
 #' 
-#' @importFrom magrittr %>%
 
 check_no_snvs_gr = function(gr){
     snv_f = find_snv(gr)
     nr_snv = sum(snv_f)
     snv_present = nr_snv >= 1
     if (snv_present){
-        stop(stringr::str_c("There seem to be ", nr_snv, " SNVs present in your data. Make sure to remove all SNVs with the remove_snvs function."), call. = F)
+        stop(stringr::str_c("There seem to be ", nr_snv, " SNVs present in your data. Make sure to remove all SNVs with the `get_mut_type` function."), call. = F)
     }
     invisible(gr)
 }
+
+
+#' Checks that there are no SNVs/MNVs.
+#'
+#' @details 
+#' This function checks for a GRanges/GRangesList object, that there are no Indels.
+#' It trows an error when this is not the case.
+#' 
+#' @param grl GRanges/GrangesList object
+#' 
+#' @return Invisibly returns its input when it succeeds.
+#' 
+check_no_indels = function(grl){
+    if (inherits(grl, "CompressedGRangesList")){
+        gr = unlist(grl)
+    } else if (inherits(grl, "GRanges")){
+        gr = grl
+    } else{
+        not_gr_or_grl(grl)
+    }
+    check_no_indels_gr(gr)
+    invisible(grl)
+}
+
+#' Checks that there are no Indels.
+#'
+#' @details 
+#' This function checks for a single GRanges object, that there are no Indels.
+#' It trows an error when this is not the case.
+#' 
+#' @param gr GRanges object
+#' 
+#' @return Invisibly returns its input when it succeeds.
+#' 
+check_no_indels_gr = function(gr){
+    snv_f = find_snv(gr)
+    nr_indel = sum(!snv_f)
+    snv_present = nr_indel >= 1
+    if (snv_present){
+        stop(str_c("There seem to be ", nr_indel, " Indels present in your data. Make sure to remove all Indels with the `get_mut_type` function."), call. = F)
+    }
+    invisible(gr)
+}
+
 
 #' Removes variants with multiple alt alleles.
 #'
@@ -104,10 +143,6 @@ check_no_snvs_gr = function(gr){
 #' @param grl GRanges/GRangesList object
 #' 
 #' @return A filtered version of the input GRanges/GRangesList object.
-#' @examples
-#' 
-#' @export
-
 remove_multi_alts_variants = function(grl){
     if (inherits(grl, "CompressedGRangesList")){
         gr_l = as.list(grl)
@@ -130,9 +165,7 @@ remove_multi_alts_variants = function(grl){
 #' @param gr GRanges object
 #' 
 #' @return A filtered version of the input GRanges object.
-#' @examples
 #' 
-
 remove_multi_alts_variants_gr = function(gr){
     alt = gr$ALT
     gr = gr[IRanges::elementNROWS(alt) == 1]
@@ -147,10 +180,6 @@ remove_multi_alts_variants_gr = function(gr){
 #' @param grl GRanges/GRangesList object
 #' 
 #' @return A filtered version of the input GRanges/GrangesList object.
-#' @examples
-#' 
-#' @export
-
 remove_snvs = function(grl){
     if (inherits(grl, "CompressedGRangesList")){
         gr_l = as.list(grl)
@@ -173,12 +202,47 @@ remove_snvs = function(grl){
 #' @param gr GRanges object
 #' 
 #' @return A filtered version of the input GRanges object.
-#' @examples
 #' 
-
 remove_snvs_gr = function(gr){
     snv_f = find_snv(gr)
     gr = gr[!snv_f]
+    return(gr)
+}
+
+#' Removes Indel variants.
+#'
+#' @details 
+#' This function removes Indel variants for a GRanges/GrangesList object.
+#' 
+#' @param grl GRanges/GRangesList object
+#' 
+#' @return A filtered version of the input GRanges/GrangesList object.
+remove_indels = function(grl){
+    if (inherits(grl, "CompressedGRangesList")){
+        gr_l = as.list(grl)
+        grl = purrr::map(gr_l, remove_indels_gr) %>% 
+            GRangesList()
+        return(grl)
+    } else if (inherits(grl, "GRanges")){
+        gr = remove_indels_gr(grl)
+        return(gr)
+    } else{
+        not_gr_or_grl(grl)
+    }
+}
+
+#' Removes Indel variants.
+#'
+#' @details 
+#' This function removes Indel variants for a single GRanges object.
+#' 
+#' @param gr GRanges object
+#' 
+#' @return A filtered version of the input GRanges object.
+#' 
+remove_indels_gr = function(gr){
+    snv_f = find_snv(gr)
+    gr = gr[snv_f]
     return(gr)
 }
 
@@ -191,9 +255,7 @@ remove_snvs_gr = function(gr){
 #' @param gr GRanges object
 #' 
 #' @return A boolean vector. It's TRUE for SNVs/MNVs and FALSE for Indels
-#' @examples
 #' 
-
 find_snv = function(gr){
     check_no_multi_alts(gr)
     snv_f = width(gr$REF) == width(unlist(gr$ALT))
@@ -208,13 +270,6 @@ find_snv = function(gr){
 #' 
 #' @param arg Argument. Any object that should have been a GRanges/GrangesList, but isn't.
 #' 
-#' @examples
-#' \dontrun{
-#' a = 1
-#' not_gr_or_grl(a)
-#' }
-#' 
-
 not_gr_or_grl = function(arg){
     arg_name = deparse(substitute(arg))
     arg_class = class(arg)[[1]]
@@ -231,9 +286,7 @@ not_gr_or_grl = function(arg){
 #' @param gr GRanges object
 #' @param ref_genome BSGenome reference genome object
 #' 
-#' @examples
 #' 
-
 check_chroms = function(gr, ref_genome){
     gr_seqnames = as.vector(seqnames(gr))
     ref_seqnames = seqnames(get(ref_genome))
