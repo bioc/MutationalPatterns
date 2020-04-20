e9dae31ff22aae62315cedb1991aefb20aac1742
FreekManders <freek.manders@gmail.com>
2020-04-20 17:29:46 +0200

    Added fake MNVs for the blood data. This is usefull for the unit tests, examples and vignette that will be made for the MNVs.

--- data-raw/create_example_vcfs.R
+++ data-raw/create_example_vcfs.R
@@ -3,7 +3,67 @@ library(VariantAnnotation)
 ref_genome = "BSgenome.Hsapiens.UCSC.hg19"
 library(ref_genome, character.only = TRUE)
 
+#Function to add fake MNVs to example data. This is usefull for the unit tests, examples and vignette.
+add_fake_mnvs = function(vcf, ref_genome, nr_mnvs = NA){
+    if (is.na(nr_mnvs)){
+        nr_mnvs = sample(seq(5,10), 1)
+    }
+    
+    mnv_lengths = sample(seq(3,10), 
+                         nr_mnvs, 
+                         replace = T, 
+                         prob = c(0.35, 0.25, 0.15, 0.1, 0.05, 0.05, 0.025, 0.025))
+    mnvs = purrr::map(mnv_lengths, create_1_mnv, vcf) %>% 
+        do.call(rbind, .) %>% 
+        sort()
+    vcf = rbind(vcf, mnvs)
+    return(vcf)
+}
 
+#helper function of add_fake_mnvs
+create_1_mnv = function(mnv_length, vcf){
+    
+    #Select random variant
+    vcf = vcf[isSNV(vcf)]
+    variant = vcf[sample.int(length(vcf), 1)]
+    
+    #Combine variant `mnv_length` times
+    mnv_variant = purrr::map(seq(1, mnv_length), function(x) return(variant)) %>% 
+        do.call(rbind, .)
+    
+    #Replace the positions in the vcf
+    first_start = start(mnv_variant)[1] + 100
+    starts = seq(first_start, first_start + (mnv_length-1))
+    end(mnv_variant) = starts
+    start(mnv_variant) = starts
+    
+    #Update the ref bases
+    gr = granges(mnv_variant)
+    old_seqnames = as.vector(seqnames(gr))
+    seqlevelsStyle(gr) = "UCSC"
+    seqlevels(gr) = str_c("chr", c(1:22, "X", "Y"))
+    refs = getSeq(get(ref_genome), gr)
+    ref(mnv_variant) = refs
+    
+    possible_alts = purrr::map(as.vector(refs), ~setdiff(c("A", "C", "T", "G"), .x))
+    alts = purrr::map(possible_alts, ~sample(.x, 1)) %>% 
+        DNAStringSetList(use.names = F)
+    alt(mnv_variant) = alts
+    
+    #Update the names of the new variants
+    names(mnv_variant) = str_c(old_seqnames, 
+                               ":", 
+                               starts, 
+                               "_", 
+                               as.vector(ref(mnv_variant)), 
+                               "/", 
+                               as.vector(unlist(alt(mnv_variant)))
+    )
+    return(mnv_variant)
+}
+
+
+#Function to remove data from a vcf, that is not needed for the package.
 decrease_vcf_size = function(vcf_fname){
     #Read vcf
     vcf = readVcf(vcf_fname)
@@ -17,12 +77,16 @@ decrease_vcf_size = function(vcf_fname){
     geno(vcf) = geno(vcf)[gt_to_keep]
     geno(header(vcf)) = geno(header(vcf))[gt_to_keep,, drop = F]
     
+    #Remove filter statements from header
+    fixed(header(vcf))[["FILTER"]] = fixed(header(vcf))[["FILTER"]][0,,drop = F]
+    
     #Remove all but one sample
     tmp_name = "tmp.vcf"
     writeVcf(vcf, tmp_name)
     system(str_c("cut -f1-10 ", tmp_name, " > ", vcf_fname))
     file.remove(tmp_name)
     invisible(0)
+    
 }
 
 
@@ -53,6 +117,10 @@ vcf_l = vcf_l %>%
     purrr::map(., function(x) do.call(rbind, x)) %>% #Combine vcfs from a single donor
     purrr::map(sort) %>% 
     purrr::map(unique)
+
+#Add fake mnvs.
+set.seed(42)
+vcf_l = purrr::map(vcf_l, add_fake_mnvs, ref_genome)
 
 #Write vcfs
 out_vcf_fnames = str_c("inst/extdata/blood-", names(vcf_l), ".vcf")
