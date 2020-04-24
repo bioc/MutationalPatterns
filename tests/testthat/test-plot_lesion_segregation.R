40bfafee457b1956481c52aadcc7de0d108b8440
FreekManders <freek.manders@gmail.com>
2020-04-24 12:41:57 +0200

    Added function to plot lesion segregation.

---
+++ tests/testthat/test-plot_lesion_segregation.R
@@ -0,0 +1,25 @@
+context("test-plot_lesion_segregation")
+
+
+# Load GRangesList
+grl <- readRDS(system.file("states/read_vcfs_as_granges_output.rds",
+                           package="MutationalPatterns"))
+
+#Select sample
+gr = grl[[1]]
+
+#Perform function
+output = plot_lesion_segregation(gr, sample_name = "Colon1")
+output_noname = plot_lesion_segregation(gr)
+output_l = plot_lesion_segregation(gr, per_chrom = T, sample_name = "Colon1")
+
+test_that("Output has correct class",{
+    expect_true(inherits(output, c("gg")))
+    expect_true(inherits(output_noname, c("gg")))
+    expect_true(inherits(output_l, c("list")))
+    expect_true(inherits(output_l[[1]], c("gg")))
+})
+
+test_that("Output per chromosome has correct length",{
+    expect_equal(length(output_l), 22)
+})
