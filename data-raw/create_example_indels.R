22439868baa267dd6f3fae304dc1453b0e6d392f
FreekManders <freek.manders@gmail.com>
2020-04-29 16:31:54 +0200

    Added function to lengthen a mutation matrix. Also works for indels and dbs.

--- data-raw/create_example_indels.R
+++ data-raw/create_example_indels.R
@@ -31,3 +31,28 @@ indel_m = indel_counts %>%
 fit_res = fit_to_signatures(indel_m, signatures)
 saveRDS(fit_res, "inst/states/indel_refit.rds")
 
+
+
+
+#Split per region
+CTCF_g <- readRDS(system.file("states/CTCF_g_data.rds",
+                              package="MutationalPatterns"))
+promoter_g <- readRDS(system.file("states/promoter_g_data.rds",
+                                  package="MutationalPatterns"))
+flanking_g <- readRDS(system.file("states/promoter_flanking_g_data.rds",
+                                  package="MutationalPatterns"))
+
+#Combine the regions into a single GRangesList
+regions <- GRangesList(promoter_g, flanking_g, CTCF_g)
+names(regions) <- c("Promoter", "Promoter flanking", "CTCF")
+
+seqlevelsStyle(regions) = "UCSC"
+grl_indel_split = split_muts_region(grl_indel_context, regions)
+indel_counts_split = count_indel_contexts(grl_indel_split)
+saveRDS(indel_counts_split, "inst/states/blood_indels_counts_split_region.rds")
+indel_m_split = indel_counts_split %>% 
+    dplyr::select(-muttype, -muttype_sub) %>% 
+    as.matrix()
+rownames(indel_m_split) = stringr::str_c(indel_counts_split$muttype, indel_counts_split$muttype_sub, sep = "_")
+indel_matrix_long = lengthen_mut_matrix(indel_m_split)
+saveRDS(indel_matrix_long, "inst/states/blood_indels_longmatrix_split_region.rds")
