b4911b352d8e85281816f28d5207e9476943d75f
FreekManders <freek.manders@gmail.com>
2020-05-29 14:41:47 +0200

    Add replication bias to the unit test of plot_strand.

--- tests/testthat/test-plot_strand.R
+++ tests/testthat/test-plot_strand.R
@@ -21,7 +21,15 @@ output = plot_strand(strand_counts)
 # Plot in absolute mode.
 output_absolute = plot_strand(strand_counts, mode = "absolute")
 
+#Repeat for replication bias.
+mut_mat_repli <- readRDS(system.file("states/mut_mat_repli.rds",
+                                     package="MutationalPatterns"))
+strand_counts = strand_occurrences(mut_mat_repli, by=tissue)
+output_repli = plot_strand(strand_counts)
+
+
 test_that("Output has correct class",{
     expect_true(inherits(output, c("gg")))
     expect_true(inherits(output_absolute, c("gg")))
+    expect_true(inherits(output_repli, c("gg")))
 })
