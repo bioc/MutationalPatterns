d010092206236ccaf55a714b91086ed80f587c06
FreekManders <freek.manders@gmail.com>
2020-06-02 18:55:30 +0200

    plot_compare_profiles is now smaller and no longer depends on global variables like CONTEXTS_96.

--- R/plot_compare_profiles.R
+++ R/plot_compare_profiles.R
@@ -47,7 +47,7 @@ plot_compare_profiles = function(profile1,
                                     profile_names = c("profile 1", "profile 2"),
                                     profile_ymax = 0.2,
                                     diff_ylim = c(-0.02, 0.02),
-                                    colors,
+                                    colors = NA,
                                     condensed = FALSE)
 {
     # These variables use non standard evaluation.
@@ -55,7 +55,11 @@ plot_compare_profiles = function(profile1,
     value = substitution = Sample = Contribution = Signature = variable = NULL
     
     # if colors parameter not provided, set to default colors
-    if(missing(colors)){colors = COLORS6}
+    if(is_na(colors)){
+      colors = COLORS6
+    }
+    
+    #Calculate relative values.
     s1_relative = profile1 / sum(profile1)
     s2_relative = profile2 / sum(profile2)
     diff = s1_relative - s2_relative
@@ -69,92 +73,61 @@ plot_compare_profiles = function(profile1,
     # round
     cosine_sim = round(cosine_sim, 3)
     
+    #Combine samples and diff.
     x = BiocGenerics::cbind(s1_relative, s2_relative, diff)
     colnames(x) = c(profile_names, "Difference")
 
-    substitutions = c('C>A', 'C>G', 'C>T', 'T>A', 'T>C', 'T>G')
-    index = c(rep(1,1,16), rep(2,1,16), rep(3,1,16),
-                rep(4,1,16), rep(5,1,16), rep(6,1,16))
-
-    # Context
-    context = CONTEXTS_96
-
-    # Replace mutated base with dot
-    substring(context,2,2) = "."
-
-    # Construct dataframe for plotting
-    df = data.frame(substitution = substitutions[index], context = context)
-    rownames(x) = NULL
-    df2 = cbind(df, as.data.frame(x))
-    df3 = df2 %>% 
+   
+    #Get substitution and context. Then make data long for plotting.
+    df = x %>%
+      as.data.frame() %>% 
+      tibble::rownames_to_column("full_context") %>% 
+      dplyr::mutate(substitution = stringr::str_replace(full_context, "\\w\\[(.*)\\]\\w", "\\1"),
+                    context = stringr::str_replace(full_context, "\\[.*\\]", "\\.")) %>% 
+      dplyr::select(-full_context) %>% 
       tidyr::pivot_longer(c(-substitution, -context), names_to = "variable", values_to = "value") %>% 
       dplyr::mutate(variable = factor(variable, levels = unique(variable)))
-    
+      
+ 
     # Add dummy non_visible data points to force y axis limits per facet
-    df4 = data.frame(substitution = rep("C>A", 4),
-                        context = rep("A.A",4),
+    df_blank = data.frame(substitution = rep(df$substitution[1], 4),
+                        context = rep(df$context[1],4),
                         variable = c(profile_names, "Difference", "Difference"),
                         value = c(profile_ymax,
                                     profile_ymax,
                                     diff_ylim[1],
                                     diff_ylim[2]))
-    if (condensed)
-    {
-      plot = ggplot(data=df3, aes(x=context,
-                                  y=value,
-                                  fill=substitution,
-                                  width=1)) +
-        geom_bar(stat="identity",
-                 position = "identity",
-                 colour="black", size=.2) +
-        geom_point(data = df4, aes(x = context,
-                                   y = value), alpha = 0) +
-        scale_fill_manual(values=colors) +
-        facet_grid(variable ~ substitution, scales = "free_y") +
-        ylab("Relative contribution") +
-        # ylim(-yrange, yrange) +
-        # no legend
-        guides(fill=FALSE) +
-        # white background
-        theme_bw() +
-        ggtitle(paste("RSS = ", RSS, "; Cosine similarity = ", cosine_sim, sep = "")) +
-        # format text
-        theme(axis.title.y=element_text(size=12,vjust=1),
-              axis.text.y=element_text(size=8),
-              axis.title.x=element_text(size=12),
-              axis.text.x=element_text(size=5,angle=90,vjust=0.4),
-              strip.text.x=element_text(size=14),
-              strip.text.y=element_text(size=14),
-              panel.grid.major.x = element_blank(),
-              panel.spacing.x = unit(0, "lines"))
-      
-    } else {
-    plot = ggplot(data=df3, aes(x=context,
+    
+    if (condensed == TRUE){
+      width = 1
+      spacing = 0
+    } else{
+      width = 0.6
+      spacing = 0.5
+    }
+    
+    plot = ggplot(data=df, aes(x=context,
                                 y=value,
                                 fill=substitution,
-                                width=0.6)) +
-        geom_bar(stat="identity",
-                    position = "identity",
-                    colour="black", size=.2) +
-        geom_point(data = df4, aes(x = context,
-                                    y = value), alpha = 0) +
-        scale_fill_manual(values=colors) +
-        facet_grid(variable ~ substitution, scales = "free_y") +
-        ylab("Relative contribution") +
-        # ylim(-yrange, yrange) +
-        # no legend
-        guides(fill=FALSE) +
-        # white background
-        theme_bw() +
-        ggtitle(paste("RSS = ", RSS, "; Cosine similarity = ", cosine_sim, sep = "")) +
-        # format text
-        theme(axis.title.y=element_text(size=12,vjust=1),
-                axis.text.y=element_text(size=8),
-                axis.title.x=element_text(size=12),
-                axis.text.x=element_text(size=5,angle=90,vjust=0.4),
-                strip.text.x=element_text(size=14),
-                strip.text.y=element_text(size=14),
-                panel.grid.major.x = element_blank())
-    }
+                                width=width)) +
+      geom_bar(stat="identity",
+               position = "identity",
+               colour="black", size=.2) +
+      geom_blank(data = df_blank, aes(x = context, y = value)) +
+      scale_fill_manual(values=colors) +
+      facet_grid(variable ~ substitution, scales = "free_y") +
+      labs(y = "Relative contribution",
+           title =paste0("RSS = ", RSS, "; Cosine similarity = ", cosine_sim)) +
+      guides(fill=FALSE) +
+      theme_bw() +
+      theme(axis.title.y=element_text(size=12,vjust=1),
+            axis.text.y=element_text(size=8),
+            axis.title.x=element_text(size=12),
+            axis.text.x=element_text(size=5,angle=90,vjust=0.4),
+            strip.text.x=element_text(size=14),
+            strip.text.y=element_text(size=14),
+            panel.grid.major.x = element_blank(),
+            panel.spacing.x = unit(spacing, "lines"))
+    
     return(plot)
 }
