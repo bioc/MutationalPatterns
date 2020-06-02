184ee03707aed1455a7b0b280b9686d52e346ae6
FreekManders <freek.manders@gmail.com>
2020-06-02 19:04:38 +0200

    Added unit test for plot_enrichment_depletion.

---
+++ tests/testthat/test-plot_enrichment_depletion.R
@@ -0,0 +1,23 @@
+context("test-plot_enrichment_depletion")
+
+#Read distribution data
+distr <- readRDS(system.file("states/distr_data.rds",
+                    package="MutationalPatterns"))
+#Set tissue
+tissue = c( "colon", "colon", "colon",
+            "intestine", "intestine", "intestine",
+            "liver", "liver", "liver" )
+
+## Perform the enrichment/depletion test.
+distr_test = enrichment_depletion_test(distr, by = tissue)
+distr_test2 = enrichment_depletion_test(distr)
+
+## Plot the enrichment/depletion
+output = plot_enrichment_depletion(distr_test)
+output_persample = plot_enrichment_depletion(distr_test2)
+
+#Perform tests
+test_that("Output has correct class",{
+    expect_true(inherits(output, c("gg")))
+    expect_true(inherits(output_persample, c("gg")))
+})
