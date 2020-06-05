484a961afd7b14a1de3319c7d555eae5109ebff2
FreekManders <freek.manders@gmail.com>
2020-06-05 18:05:21 +0200

    Added variation_bayes method to unit test of extract_signatures.

--- tests/testthat/test-extract_signatures.R
+++ tests/testthat/test-extract_signatures.R
@@ -13,15 +13,20 @@ indel_counts <- readRDS(system.file("states/blood_indel_counts.rds",
                                package="MutationalPatterns"))
 output_indel = extract_signatures(indel_counts, rank = 2, nrun = 1)
 
+#Check that the variational bayes method works
+output_bayes <- extract_signatures(mut_mat, rank = 2, nrun = 1, nmf_type = "variational_bayes")
+
 #Tests
 test_that("Outputs a list", {
     expect_true(inherits(output, c("list")))
     expect_true(inherits(output_indel, c("list")))
+    expect_true(inherits(output_bayes, c("list")))
 })
 
 test_that("Output contains signatures, contribution and reconstructed", {
     expect_identical(names(output), c("signatures", "contribution", "reconstructed"))
     expect_identical(names(output_indel), c("signatures", "contribution", "reconstructed"))
+    expect_identical(names(output_bayes), c("signatures", "contribution", "reconstructed"))
 })
 
 test_that("Output elements are matrixes", {
@@ -31,6 +36,9 @@ test_that("Output elements are matrixes", {
     expect_true(inherits(output_indel$signatures, "matrix"))
     expect_true(inherits(output_indel$contribution, "matrix"))
     expect_true(inherits(output_indel$reconstructed, "matrix"))
+    expect_true(inherits(output_bayes$signatures, "matrix"))
+    expect_true(inherits(output_bayes$contribution, "matrix"))
+    expect_true(inherits(output_bayes$reconstructed, "matrix"))
 })
 
 test_that("Output elements have correct size", {
@@ -40,4 +48,7 @@ test_that("Output elements have correct size", {
     expect_equal(dim(output_indel$signatures), c(83, 2))
     expect_equal(dim(output_indel$contribution), c(2, 3))
     expect_equal(dim(output_indel$reconstructed), c(83, 3))
+    expect_equal(dim(output_bayes$signatures), c(96, 2))
+    expect_equal(dim(output_bayes$contribution), c(2, 9))
+    expect_equal(dim(output_bayes$reconstructed), c(96, 9))
 })
